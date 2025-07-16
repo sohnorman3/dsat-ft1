ff8a4432766f3bc2b63f05e2a09297c364cd322b
sohnorman3 <118236031+sohnorman3@users.noreply.github.com>
2025-07-16 05:52:09 +0000

    sync8

--- app.py
+++ app.py
@@ -127,6 +127,23 @@ def webhook():
         })
     return('ok', 200)
 
+@app.route("/stop_telegram",methods=["GET","POST"])
+def stop_telegram():
+
+    domain_url = 'https://dsat-ft1-h4gs.onrender.com'
+
+    # The following line is used to delete the existing webhook URL for the Telegram bot
+    delete_webhook_url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/deleteWebhook"
+    webhook_response = requests.post(delete_webhook_url, json={"url": domain_url, "drop_pending_updates": True})
+
+    if webhook_response.status_code == 200:
+        # set status message
+        status = "The telegram bot is stopped. "
+    else:
+        status = "Failed to stop the telegram bot. Please check the logs."
+    
+    return(render_template("telegram.html", status=status))
+
 if __name__ == "__main__":
     app.run()
 
